97fa03169a0d343b16ef3f236e807b3b25b35c1e
Amy Liu <al996@cornell.edu>
2022-05-03 15:30:43 -0400

    replace svg with heatmap with test data

--- www/static/keypoints.js
+++ www/static/keypoints.js
@@ -1,12 +1,12 @@
 let keypoints = document.getElementById("keypoints").innerHTML += "<div><svg></svg></div>"
 keypoints.innerHTML += "<div><svg></svg></div>"
 
-function addKeypoint(title, divname, svgname, paragraph1, paragraph2){
+function addKeypoint(title, divname, heatmapname, paragraph1, paragraph2) {
     let html = document.getElementById("keypoints")
-    html.innerHTML += "<div class='case' id='"+divname+"'></div>"
+    html.innerHTML += "<div class='case' id='" + divname + "'></div>"
     let div = document.getElementById(divname)
-    div.innerHTML += "<h3>"+title+"</h3>"
-    div.innerHTML += "<p>"+paragraph1+"</p>"
-    div.innerHTML += "<svg id='"+svgname+"' width='500' height='500' ></svg>"
-    div.innerHTML += "<p>"+paragraph2+"</p>"
+    div.innerHTML += "<h3>" + title + "</h3>"
+    div.innerHTML += "<p>" + paragraph1 + "</p>"
+    div.innerHTML += "<div id='" + heatmapname + "'></div>"
+    div.innerHTML += "<p>" + paragraph2 + "</p>"
 }
