c5f69e95d9797c91932e6443c599d996643632ff
Faris42 <fsa22@cornell.edu>
2022-05-06 07:25:17 -0400

    Finished preprocessing

---
+++ generate_heatmap_combine.py
@@ -0,0 +1,96 @@
+import numpy as np
+import pickle
+import seaborn as sns
+import matplotlib.pyplot as plt
+import gc
+
+# Load pickle with 3D array of pixel values.
+
+t = 2
+c = 2
+name = "100"
+
+with open(f'data/compiled/t{t}-c{c}.p','rb') as f:
+    arr = pickle.load(f)
+
+# Compute changes
+left = arr[:-1]
+right = arr[1:]
+diff = left-right
+
+print(diff.shape)
+
+del arr
+gc.collect()
+
+# Grab a subset of the data if you want
+# sample = diff[ :, 497:528, 175:244] #canada
+# sample = diff[188:3010, (1733-1000):(1947-1000), 540:714] #Dutch 
+# sample = diff[2262:2498, 665:714, (1832-1000):(1888-1000)] #Minecraft
+sample = diff[6:3010, 300:700, 940:1000]
+print(sample.shape)
+
+# This is the part that takes the longest
+changes = sample != 0
+
+# Sum changes through all time-steps
+n_updates1 = changes.sum(axis=0)
+print(n_updates1.shape)
+
+# Normalize the distribution of pixel values so it looks more legible
+def normalizer(n, k=1):
+    return np.log(n+k)
+
+norm_updates1 = normalizer(n_updates1, k=5)
+
+# -------------------------------------------------------------------------
+
+t = 2
+c = 3
+
+with open(f'data/compiled/t{t}-c{c}.p','rb') as f:
+    arr = pickle.load(f)
+
+# Compute changes
+left = arr[:-1]
+right = arr[1:]
+diff = left-right
+
+print(diff.shape)
+
+del arr
+gc.collect()
+
+# Grab a subset of the data if you want
+# sample = diff[ :, 497:528, 175:244] #canada
+# sample = diff[188:3010, (1733-1000):(1947-1000), 540:714] #Dutch 
+# sample = diff[2262:2498, 665:714, (1832-1000):(1888-1000)] #Minecraft
+sample = diff[6:3010, 300:700, 0:80]
+print(sample.shape)
+
+# This is the part that takes the longest
+changes = sample != 0
+
+# Sum changes through all time-steps
+n_updates2 = changes.sum(axis=0)
+print(n_updates2.shape)
+
+# Normalize the distribution of pixel values so it looks more legible
+def normalizer(n, k=1):
+    return np.log(n+k)
+
+norm_updates2 = normalizer(n_updates2, k=5)
+
+n_arr = [n_updates1, n_updates2]
+norm_arr = [norm_updates1, norm_updates2]
+
+n_updates = np.hstack(n_arr)
+norm_updates = np.hstack(norm_arr)
+
+print(n_updates.shape)
+
+# Save these values to CSV files
+# np.savetxt(f"data/compiled/dutch_n_updates_t{t}-c{c}.csv", n_updates.astype(int), delimiter=',', fmt='%.0f')
+# np.savetxt(f"data/compiled/dutch_norm_updates_t{t}-c{c}.csv", norm_updates, delimiter=',', fmt='%.4f')
+np.savetxt(f"data/compiled/void_n_updates_" + name + ".csv", n_updates.astype(int), delimiter=',', fmt='%.0f')
+np.savetxt(f"data/compiled/void_norm_updates_" + name + ".csv", norm_updates, delimiter=',', fmt='%.4f')
---
+++ image_slice_combine.py
@@ -0,0 +1,61 @@
+from sklearn.datasets import load_sample_image
+import numpy as np
+from PIL import Image
+
+t = 2 # Type
+c = 2 # Quadrant
+
+png_name = "2-1649111825.png" # Pick a png
+
+# x1 and y1 are top left corner, x2 and y2 are bottom right corner
+x1 = 940
+y1 = 300
+x2 = 1000
+y2 = 700
+
+output_name = "Void-100" # Final name
+
+if t==0:
+    name1 = "data/img_2022/images_single/"
+elif t==1:
+    name1 = f"data/img_2022/images_double/{c}/"
+elif t==2:
+    name1 = f"data/img_2022/images_quadro/{c}/"
+
+name = name1 + png_name
+
+image = Image.open(name)
+
+portion1 = image.crop((x1, y1, x2, y2))
+
+# ------------------------------------------------------------------ Image 2
+
+t = 2 # Type
+c = 3 # Quadrant
+
+png_name = "3-1649111825.png" # Pick a png
+
+# x1 and y1 are top left corner, x2 and y2 are bottom right corner
+x1 = 0
+x2 = 80
+
+if t==0:
+    name1 = "data/img_2022/images_single/"
+elif t==1:
+    name1 = f"data/img_2022/images_double/{c}/"
+elif t==2:
+    name1 = f"data/img_2022/images_quadro/{c}/"
+
+name = name1 + png_name
+
+image = Image.open(name)
+
+portion2 = image.crop((x1, y1, x2, y2))
+
+# use in different section
+
+concated_image = Image.new('RGB', (portion1.width + portion2.width, portion1.height))
+concated_image.paste(portion1, (0, 0))
+concated_image.paste(portion2, (portion1.width, 0))
+
+concated_image.save('data/sliced/' + output_name + '.png')
