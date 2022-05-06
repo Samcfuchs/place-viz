69095fe85249196e14a1b604b28ff5897a0ab892
Julia Atkins <jea255@cornell.edu>
2022-05-06 10:15:05 -0400

    style gallery

--- www/static/keypoints.js
+++ www/static/keypoints.js
@@ -1,24 +1,28 @@
+function encodeHTML(s) {
+    return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
+}
+
 function addKeypoint(title, divname, heatmapname, imglist, paragraph1, paragraph2) {
     let html = document.getElementById("keypoints")
     html.innerHTML += "<div class='case' id='" + divname + "'></div>"
     let div = document.getElementById(divname)
     div.innerHTML += "<h3>" + title + "</h3>"
-    div.innerHTML += "<p>" + paragraph1 + "</p>"
+    div.innerHTML += "<p>" + encodeHTML(paragraph1) + "</p>"
     div.innerHTML += "<div class='heatmap' id='" + heatmapname + "'></div>"
-    div.innerHTML += "<p>" + paragraph2 + "</p>"
+    div.innerHTML += "<p>" + encodeHTML(paragraph2) + "</p>"
     if(imglist.length>0){
+        html.innerHTML += "<div class='gallery' id='gallery" + divname + "'></div>"
+        let gal_div = document.getElementById('gallery'+divname)
         num_imgs = imglist.length
         img_counter = 1
         console.log(imglist)
         imglist.forEach(img => {
-            div.innerHTML += "<div class='imgandtext' id='" + divname + img_counter + "img'></div>"
+            gal_div.innerHTML += "<div class='imgandtext' id='" + divname + img_counter + "img'></div>"
             let imgdiv = document.getElementById(divname + img_counter+'img')
             imgdiv.innerHTML += "<img src='images/"+img[0]+"'>"
             imgdiv.innerHTML += "<p>" + img[1] + "</p>"
-            if(img_counter%2 == 0){
-                document.getElementById(divname + img_counter+'img').classList.add("reverse")
-            }
             img_counter+=1
         });
     }
 }
+
